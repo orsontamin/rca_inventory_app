c32be08a51bf684f9dc211f25ceefb26617c0db9
Orson <orson.t99@gmail.com>
2022-02-08 06:35:34 +0800

    Create admin table

---
+++ db/migrate/20220207121413_create_admins_table.rb
@@ -0,0 +1,9 @@
+class CreateAdminsTable < ActiveRecord::Migration[7.0]
+  def change
+    create_table :admins do |t|
+      t.string :username
+      t.string :email
+      t.string :password_digest
+    end
+  end
+end
