b448114b5ea6234e800ebe576f95587a23cebe44
Orson <orson.t99@gmail.com>
2022-02-08 12:04:21 +0800

    Implement sign in,sign out, and dashboards for admin

--- myapp.rb
+++ myapp.rb
@@ -1,22 +1,34 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
-
 require "sinatra/activerecord"
 require './models/user'
+require './models/admin'
 
 enable :sessions
 
 helpers do
   def log_in(user)
-    session[:user_id] = user.id
+     session[:user_id] = user.id
+   end
+
+  def admin_log_in(user)
+    session[:admin_id] = user.id
   end
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
+  def admin
+    @admin = Admin.find_by(id: session[:admin_id])
+  end
+
   def user_signed_in?
     !current_user.nil?
+  end
+
+  def admin_signed_in?
+    !admin.nil?
   end
 end
 
@@ -25,41 +37,38 @@ get '/' do
  erb :index
 end
 
+####### ######## ########
+####### USERS ##########
+####### ######## ########
 get '/sign-in' do
+  if user_signed_in?
+    redirect '/'
+  end
   erb :sign_in
 end
 
+post '/sign-out' do
+  session.clear
+  @current_user || @admin = nil
+  redirect '/'
+end
+
 post '/sign-in' do
-  @user= User.find_by(email: params[:email])
+  @user = User.find_by(email: params[:email])
   if @user && @user.authenticate(params[:password])
     log_in(@user)
-    redirect '/dashboard'
+    redirect '/user-dashboard'
   else
     erb :sign_in
   end
 end
 
-get '/sign-out' do
-
-end
-
-post '/sign-out' do
-  session.clear
-  @current_user = nil
-  redirect '/'
-end
-
-get '/dashboard' do
+get '/user-dashboard' do
   if user_signed_in?
-    erb :dashboard
+    erb :user_dashboard
   else
     redirect '/sign-in'
   end
-end
-
-get '/sign-up' do
-  @user = User.new
-  erb :sign_up
 end
 
 post '/sign-up' do
@@ -71,11 +80,48 @@ post '/sign-up' do
 
   if @user.save
     log_in(@user)
-    redirect '/dashboard'
-
+    redirect '/user-dashboard'
   else
+    # flash[:alert_warning] = "There was an error in saving your account. Please try again"
     erb :sign_up
   end
+end
+
+####### ######## ########
+####### ADMIN ##########
+####### ######## ########
+get '/admin-sign-in' do
+  if admin_signed_in?
+    redirect '/admin-dashboard'
+  else
+    erb :admin_sign_in
+  end
+end
+
+post '/admin-sign-in' do
+  @admin = Admin.find_by(email: params[:email])
+  if @admin && @admin.authenticate(params[:password])
+    admin_log_in(@admin)
+    redirect '/admin-dashboard'
+  else
+    erb :admin_sign_in
+  end
+end
 
 
+get '/admin-dashboard' do
+  if admin_signed_in?
+    erb :admin_dashboard
+  else
+    redirect '/admin-sign-in'
+  end
+end
+
+get '/sign-up' do
+  if user_signed_in?
+    redirect '/'
+  else
+    @user = User.new
+    erb :sign_up
+  end
 end
