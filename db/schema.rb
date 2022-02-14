524ff5215a23119c3bf45856bdf79d608d6ef925
Orson <orson.t99@gmail.com>
2022-02-14 15:06:10 +0800

    Add booking status to bookings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_09_025504) do
+ActiveRecord::Schema.define(version: 2022_02_10_151100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2022_02_09_025504) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "quantity"
+    t.string "booking_status"
   end
 
   create_table "products", force: :cascade do |t|
